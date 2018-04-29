640f1d2efbb4b747a857d786fddaa0024c5539f8
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2018-04-29 20:52:21 +0900

    add UIFont+Extension

---
+++ Bet/Extensions/UIFont+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UIFont+Extension.swift
+//  Bet
+//
+//  Created by admin on 2018/04/29.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    
+    class func athelas(size: CGFloat) -> UIFont {
+        return UIFont(name: "Athelas-Regular", size: size)!
+    }
+    
+    class func hiragino(size: CGFloat) -> UIFont {
+        return UIFont(name: "HiraginoSans-W3", size: size)!
+    }
+    
+    class func hiraginoBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "HiraginoSans-W6", size: size)!
+    }
+    
+}
+
